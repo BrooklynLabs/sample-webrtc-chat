79f7cde2a90891ac51eaa1820aa0c7bb358f5ba9
eashtian <eashtian@EASHTIAN-MOBL1.amr.corp.intel.com>
2015-01-28 14:18:57 -0800

    update projectTypeName string

--- www/js/sample.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('.uib_w_1, .ad_webrtc').hide();
-
-  $('.login .webrtcbutton').click(function(){
-    $('.uib_w_2').hide();
-    $('.uib_w_1, .ad_webrtc').show();
-  });
-});
